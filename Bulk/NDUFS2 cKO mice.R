9acb2011b34d0550341ffe9603b704b89b33b9f2
YoungJin4660 <102126516+YoungJin4660@users.noreply.github.com>
2023-03-06 19:14:06 +0900

    Update NDUFS2 cKO mice.R

--- Bulk/NDUFS2 cKO mice.R
+++ Bulk/NDUFS2 cKO mice.R
@@ -19,18 +19,18 @@ sex = factor(sample_info$Sex, levels = c('F', 'M'))
 
 genes=rownames(rawdata)
 
-y <- DGEList(counts=rawdata, genes=genes, group=class)
-y_filtered_norm <- calcNormFactors(y[filterByExpr(y),keep.lib.sizes = FALSE])
+y = DGEList(counts=rawdata, genes=genes, group=class)
+y_filtered_norm = calcNormFactors(y[filterByExpr(y),keep.lib.sizes = FALSE])
 
-dge_design <- model.matrix(~sex + class)
-y_dsp <- estimateDisp(y_filtered_norm, dge_design, robust = TRUE)
-y_qlf_test <- glmQLFit(y_dsp, dge_design, robust = TRUE)
-y_qlf_test <- glmQLFTest(y_qlf_test)
+dge_design = model.matrix(~sex + class)
+y_dsp = estimateDisp(y_filtered_norm, dge_design, robust = TRUE)
+y_qlf_test = glmQLFit(y_dsp, dge_design, robust = TRUE)
+y_qlf_test = glmQLFTest(y_qlf_test)
 
 sample_info$Sex = factor(sample_info$Sex, levels = c('F','M'))
 sample_info$Group = factor(sample_info$Group, levels = c('NDUFS2 control','NDUFS2 control/NDI1','NDUFS2 cKO','NDUFS2 cKO/NDI1'))
 
-group_annot <- sample_info %>% arrange(Group)
+group_annot = sample_info %>% arrange(Group)
 group_annot = group_annot[order(group_annot$Group, group_annot$Sex),]
 
 rownames(group_annot) = group_annot$Sample.name
@@ -38,17 +38,17 @@ group_annot = group_annot[,-1]
 
 ann_colors = list(Group = c('NDUFS2 control' = '#F8766D', 'NDUFS2 control/NDI1' = '#00BFC4','NDUFS2 cKO' = '#7CAE00', 'NDUFS2 cKO/NDI1' = '#C77CFF'), Sex = c('F' = '#818181', 'M' = '#C0BEBF'))
 
-isr_sum <- decideTests(y_qlf_test, p.value = 1)
+isr_sum = decideTests(y_qlf_test, p.value = 1)
 
-isr_counts <- as.logical(abs(isr_sum)) %>%
+isr_counts = as.logical(abs(isr_sum)) %>%
   y_filtered_norm[.,] %>%
   edgeR::cpm(.) %>%
   as.data.frame() %>% 
   dplyr::select(rownames(group_annot))
   
 Atf=isr_counts[c('Atf3','Atf4','Atf5','Atf6','Ddit3'),]
-newnames <- lapply(rownames(Atf),function(x) bquote(italic(.(x))))   
-colors <- rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
+newnames = lapply(rownames(Atf),function(x) bquote(italic(.(x))))   
+colors = rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
 pheatmap(isr_counts[c('Atf3','Atf4','Atf5','Atf6','Ddit3'),],
          annotation_col = dplyr::select(group_annot, c(Group,Sex)),
          annotation_colors = ann_colors,
@@ -73,7 +73,7 @@ isr = read.table('.//isr.txt')
 isr = isr$V1 %>% unique()
 isr_heatmap = isr_counts[isr,]
 isr_heatmap = na.omit(isr_heatmap)
-newnames <- lapply(isr,function(x) bquote(italic(.(x))))  
+newnames = lapply(isr,function(x) bquote(italic(.(x))))  
 pheatmap(isr_heatmap,
          annotation_col = dplyr::select(group_annot, c(Group,Sex)),
          annotation_colors = ann_colors,
