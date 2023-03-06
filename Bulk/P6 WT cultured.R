e435673ce47f0b1cac9bedbae669f045741a5924
YoungJin4660 <102126516+YoungJin4660@users.noreply.github.com>
2023-03-06 19:58:50 +0900

    Update P6 WT cultured.R

--- Bulk/P6 WT cultured.R
+++ Bulk/P6 WT cultured.R
@@ -17,21 +17,20 @@ cul = factor(sample_info$cul, levels = c('0', '72'))
 
 genes=rownames(rawdata)
 
-y <- DGEList(counts=rawdata, genes=genes, group=class)
-y_filtered_norm <- calcNormFactors(y[filterByExpr(y),keep.lib.sizes = FALSE])
+y = DGEList(counts=rawdata, genes=genes, group=class)
+y_filtered_norm = calcNormFactors(y[filterByExpr(y),keep.lib.sizes = FALSE])
 
-dge_design <- model.matrix(~cul + class)
-y_dsp <- estimateDisp(y_filtered_norm, dge_design, robust = TRUE)
-y_qlf_test <- glmQLFit(y_dsp, dge_design, robust = TRUE)
-y_qlf_test <- glmQLFTest(y_qlf_test)
+dge_design = model.matrix(~cul + class)
+y_dsp = estimateDisp(y_filtered_norm, dge_design, robust = TRUE)
+y_qlf_test = glmQLFit(y_dsp, dge_design, robust = TRUE)
+y_qlf_test = glmQLFTest(y_qlf_test)
 
 sample_info$Sex = factor(sample_info$Sex, levels = c('F','M'))
 sample_info$Group = factor(sample_info$Group, levels = c('WT','P'))
 sample_info$ori = sample_info$ori %>% as.character()
 sample_info$ori = factor(sample_info$ori, levels = c('2','3','1','4'))
 
-reg_sum <- decideTests(y_qlf_test)
-group_annot <- sample_info %>% arrange(Group)
+group_annot = sample_info %>% arrange(Group)
 group_annot = group_annot[order(group_annot$ori, group_annot$Group),]
 
 rownames(group_annot) = group_annot$Sample.name
@@ -41,17 +40,17 @@ group_annot$cultured = group_annot$cultured %>% as.character
 colnames(group_annot)[4] = 'state'
 ann_colors = list(Group = c('WT' = '#F8766D', 'P' = '#00BFC4'), state = c( '0' = '#C0BEBF', '72' = '#818181'))
 
-isr_sum <- decideTests(y_qlf_test, p.value = 1)
+isr_sum = decideTests(y_qlf_test, p.value = 1)
 
-isr_counts <- as.logical(abs(isr_sum)) %>%
+isr_counts = as.logical(abs(isr_sum)) %>%
   y_filtered_norm[.,] %>%
   edgeR::cpm(.) %>%
   as.data.frame() %>% 
   dplyr::select(rownames(group_annot))
   
 scmar = isr_counts[c('Mki67','Cdkn1a','Krt8','Krt18','Krt7','Krt19','Sftpc','Lyz2','Sftpd','Sftpb','Abca3','Aqp5','Hopx','Vegfa'),]
-newnames <- lapply(rownames(scmar),function(x) bquote(italic(.(x)))) 
-colors <- rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
+newnames = lapply(rownames(scmar),function(x) bquote(italic(.(x)))) 
+colors = rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
 pheatmap(isr_counts[c('Mki67','Cdkn1a','Krt8','Krt18','Krt7','Krt19','Sftpc','Lyz2','Sftpd','Sftpb','Abca3','Aqp5','Hopx','Vegfa'),],
          annotation_col = dplyr::select(group_annot, c(Group,state)),
          annotation_colors = ann_colors,
@@ -72,8 +71,8 @@ pheatmap(isr_counts[c('Mki67','Cdkn1a','Krt8','Krt18','Krt7','Krt19','Sftpc','Ly
          color = colors)
          
 Atf=isr_counts[c('Atf3','Atf4','Atf5','Atf6'),]
-newnames <- lapply(rownames(Atf),function(x) bquote(italic(.(x))))   
-colors <- rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
+newnames = lapply(rownames(Atf),function(x) bquote(italic(.(x))))   
+colors = rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
 pheatmap(isr_counts[c('Atf3','Atf4','Atf5','Atf6'),],
          annotation_col = dplyr::select(group_annot, c(Group,state)),
          annotation_colors = ann_colors,
@@ -93,11 +92,11 @@ pheatmap(isr_counts[c('Atf3','Atf4','Atf5','Atf6'),],
          scale = 'row',
          color = colors)
          
-isr = read.table('./Bulk/isr.txt')
+isr = read.table('./isr.txt')
 isr = isr$V1 %>% unique()
 isr_heatmap = isr_counts[isr,]
 isr_heatmap = na.omit(isr_heatmap)
-newnames <- lapply(isr,function(x) bquote(italic(.(x))))  
+newnames = lapply(isr,function(x) bquote(italic(.(x))))  
 pheatmap(isr_heatmap,
          annotation_col = dplyr::select(group_annot, c(Group,state)),
          annotation_colors = ann_colors,
