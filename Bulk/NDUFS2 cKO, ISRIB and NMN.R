7a90139cfc7d7980ed264cb7ffb2a7a63f523768
YoungJin4660 <102126516+YoungJin4660@users.noreply.github.com>
2023-03-06 19:32:11 +0900

    Update NDUFS2 cKO, ISRIB and NMN.R

--- Bulk/NDUFS2 cKO, ISRIB and NMN.R
+++ Bulk/NDUFS2 cKO, ISRIB and NMN.R
@@ -12,7 +12,7 @@ rawdata %>% head
 rawdata = rawdata %>% as.matrix()
 batch = c(rep(1,30),rep(2,64))
 
-adjusted_counts <- ComBat_seq(rawdata, batch=batch, group=NULL, covar_mod=NULL)
+adjusted_counts = ComBat_seq(rawdata, batch=batch, group=NULL, covar_mod=NULL)
 write.table(adjusted_counts, "./ndufs2_isrib_nmn/adjusted_counts.tsv",sep = '\t', quote = F)
 
 
@@ -29,16 +29,16 @@ class = colData$condition
 genes=rownames(all_count)
 
 
-y <- DGEList(counts=all_count, genes=genes, group=class)
+y = DGEList(counts=all_count, genes=genes, group=class)
 
-y_filtered_norm <- calcNormFactors(y[filterByExpr(y),keep.lib.sizes = FALSE])
+y_filtered_norm = calcNormFactors(y[filterByExpr(y),keep.lib.sizes = FALSE])
 
-dge_design <- model.matrix(~Sex + class)
-y_dsp <- estimateDisp(y_filtered_norm, dge_design, robust = TRUE)
-y_qlf_test <- glmQLFit(y_dsp, dge_design, robust = TRUE)
-y_qlf_test <- glmQLFTest(y_qlf_test)
+dge_design = model.matrix(~Sex + class)
+y_dsp = estimateDisp(y_filtered_norm, dge_design, robust = TRUE)
+y_qlf_test = glmQLFit(y_dsp, dge_design, robust = TRUE)
+y_qlf_test = glmQLFTest(y_qlf_test)
 
-group_annot <- sample_info %>% arrange(Group)
+group_annot = sample_info %>% arrange(Group)
 group_annot$Group = factor(group_annot$Group, levels = c('WT','NDUFS2 control','NDUFS2 control + ISRIB','NDUFS2 control + NMN','NDUFS2 control/NDI1','NDUFS2 cKO','NDUFS2 cKO + ISRIB','NDUFS2 cKO + NMN','NDUFS2 cKO/NDI1'))
 group_annot$Sex = factor(group_annot$Sex, levels = c('F','M'))
 group_annot = group_annot[order(group_annot$Group, group_annot$Sex),]
@@ -50,17 +50,17 @@ ann_colors = list(Group = c('WT' = '#000000', 'NDUFS2 control' = '#44000A', 'NDU
                             'NDUFS2 cKO' = '#005174', 'NDUFS2 cKO + ISRIB' = '#007AAE', 'NDUFS2 cKO + NMN' = '#60C5F1', 'NDUFS2 cKO/NDI1' = '#94D8F6'), Sex = c('F' = '#818181', 'M' = '#C0BEBF'))
 
 
-isr_sum <- decideTests(y_qlf_test, p.value = 1)
+isr_sum = decideTests(y_qlf_test, p.value = 1)
 
-isr_counts <- as.logical(abs(isr_sum)) %>%
+isr_counts = as.logical(abs(isr_sum)) %>%
   y_filtered_norm[.,] %>%
   edgeR::cpm(.) %>%
   as.data.frame() %>% 
   dplyr::select(rownames(group_annot)) 
   
 Atf=isr_counts[c('Atf3','Atf4','Atf5','Atf6'),]
-newnames <- lapply(rownames(Atf),function(x) bquote(italic(.(x))))   
-colors <- rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
+newnames = lapply(rownames(Atf),function(x) bquote(italic(.(x))))   
+colors = rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
 pheatmap(isr_counts[c('Atf3','Atf4','Atf5','Atf6'),],
          annotation_col = dplyr::select(group_annot, c(Group,Sex)),
          annotation_colors = ann_colors,
@@ -82,8 +82,8 @@ pheatmap(isr_counts[c('Atf3','Atf4','Atf5','Atf6'),],
          color = colors)
          
 ndu=isr_counts[c('Ndufs2'),]
-newnames <- lapply(rownames(ndu),function(x) bquote(italic(.(x))))   
-colors <- rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
+newnames = lapply(rownames(ndu),function(x) bquote(italic(.(x))))   
+colors = rev(colorRampPalette(RColorBrewer::brewer.pal(10, "RdBu"))(256))
 pheatmap(isr_counts[c('Ndufs2'),],
          annotation_col = dplyr::select(group_annot, c(Group,Sex)),
          annotation_colors = ann_colors,
@@ -109,7 +109,7 @@ isr = isr$V1 %>% unique()
 isr_heatmap = isr_count[isr,]
 isr_heatmap = na.omit(isr_heatmap)
 
-newnames <- lapply(isr,function(x) bquote(italic(.(x))))  
+newnames = lapply(isr,function(x) bquote(italic(.(x))))  
 pheatmap(isr_heatmap,
          annotation_col = dplyr::select(group_annot, c(Group,Sex)),
          annotation_colors = ann_colors,
